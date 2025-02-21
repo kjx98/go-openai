0117a955989519b91e4191c12b1828642e9008a9
Jesse Kuang <jkuang@21cn.com>
2025-02-22 01:35:27 +0800

    update chat_stream example fix typo

--- examples/chat_stream/main.go
+++ examples/chat_stream/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"context"
 	"errors"
+	"flag"
 	"fmt"
 	"io"
 	"os"
@@ -11,12 +12,65 @@ import (
 	"github.com/sashabaranov/go-openai"
 )
 
+var (
+	platName  string
+	apiKey    string
+	modelName string
+	baseUrl   string
+	bVerbose  bool
+)
+
+type platFormType struct {
+	baseUrl   string
+	modelName string
+}
+
+// "gemini-2.0-flash-thinking-exp-01-21"
+var platforms = map[string]platFormType{
+	"tencent": {"https://api.lkeap.cloud.tencent.com/v1", "deepseek-r1"},
+	"aliyun":  {"https://dashscope.aliyuncs.com/compatible-mode/v1", "deepseek-r1"},
+	"groq":    {"https://api.groq.com/openai/v1", "llama-3.3-70b-versatile"},
+	"gemini": {"https://generativelanguage.googleapis.com/v1beta/openai",
+		"gemini-2.0-flash-thinking-exp"},
+	"siliconflow": {"https://api.siliconflow.cn/v1", "deepseek-ai/DeepSeek-R1"},
+	"deepseek":    {"https://api.deepseek.com/v1", "deepseek-reasoner"},
+}
+
 func main() {
-	cfg := openai.DefaultConfig(os.Getenv("LLM_API_KEY"))
-	baseUrl := os.Getenv("LLM_BASE_URL")
-	model := os.Getenv("LLM_MODEL")
-	if model == "" {
-		model = "deepseek-r1"
+	flag.StringVar(&platName, "plat", "", "platform name")
+	flag.StringVar(&apiKey, "apikey", "", "LLM API_KEY")
+	flag.StringVar(&modelName, "model", "", "LLM model name")
+	flag.StringVar(&baseUrl, "baseURI", "", "openai API baseURI")
+	flag.BoolVar(&bVerbose, "v", false, "verbose log")
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "usage: auction [options]\n")
+		flag.PrintDefaults()
+		os.Exit(2)
+	}
+	flag.Parse()
+
+	if platName != "" {
+		if plat, ok := platforms[platName]; ok {
+			if baseUrl == "" {
+				baseUrl = plat.baseUrl
+			}
+			if modelName == "" {
+				modelName = plat.modelName
+			}
+		}
+	}
+	if apiKey == "" {
+		apiKey = os.Getenv("LLM_API_KEY")
+	}
+	cfg := openai.DefaultConfig(apiKey)
+	if baseUrl == "" {
+		baseUrl = os.Getenv("LLM_BASE_URL")
+	}
+	if modelName == "" {
+		modelName = os.Getenv("LLM_MODEL")
+		if modelName == "" {
+			modelName = "deepseek-r1"
+		}
 	}
 	if baseUrl == "" {
 		//cfg.BaseURL = "https://api.deepseek.com/v1"
@@ -27,15 +81,11 @@ func main() {
 	client := openai.NewClientWithConfig(cfg)
 
 	req := openai.ChatCompletionRequest{
-		Model: model,
+		Model: modelName,
 		Messages: []openai.ChatCompletionMessage{
 			{
 				Role:    openai.ChatMessageRoleSystem,
 				Content: "you are a helpful chatbot",
-			},
-			{
-				Role:    openai.ChatMessageRoleSystem,
-				Content: "使用简体中文回答，符合简体中文的表达习惯",
 			},
 		},
 		Stream: true,
@@ -48,6 +98,9 @@ func main() {
 		if s.Text() == "/bye" {
 			break
 		}
+		if s.Text() == "" {
+			continue
+		}
 		req.Messages = append(req.Messages, openai.ChatCompletionMessage{
 			Role:    openai.ChatMessageRoleUser,
 			Content: s.Text(),
@@ -55,39 +108,51 @@ func main() {
 		stream, err := client.CreateChatCompletionStream(context.Background(), req)
 		if err != nil {
 			fmt.Printf("ChatCompletionStream error: %v\n", err)
-			continue
+			break
 		}
 		isDone := false
 		isAnswering := false
-		fmt.Println("====thinking===")
+		hasReasoning := false
+		var usage *openai.Usage
 		for !isDone {
 			resp, streamErr := stream.Recv()
 			if errors.Is(streamErr, io.EOF) {
 				break
 			}
-			if len(resp.Choices) == 0 {
-				// should be include Usage
-				if resp.Usage != nil {
-					fmt.Printf("PromptTokens: %d, CompletionTokens: %di, Total: %d\n",
-						resp.Usage.PromptTokens, resp.Usage.CompletionTokens,
-						resp.Usage.TotalTokens)
+			// should be include Usage
+			if resp.Usage != nil {
+				usage = resp.Usage
+			}
+			for _, choice := range resp.Choices {
+				if choice.Delta.Content == "" &&
+					choice.Delta.ReasoningContent == "" {
+					// process blank
+					continue
+				}
+				if choice.Delta.ReasoningContent != "" && !hasReasoning {
+					hasReasoning = true
+					fmt.Println("====thinking===")
+				}
+				if choice.Delta.ReasoningContent == "" && !isAnswering {
+					if hasReasoning {
+						fmt.Printf("\n====result content===\n")
+					}
+					isAnswering = true
+				}
+				if choice.Delta.ReasoningContent != "" {
+					fmt.Printf("%s", choice.Delta.ReasoningContent)
+				}
+				if choice.Delta.Content != "" {
+					fmt.Printf("%s", choice.Delta.Content)
 				}
 			}
-			if resp.Choices[0].Delta.Content == "" &&
-				resp.Choices[0].Delta.ReasoningContent == "" {
-				// process blank
-				continue
-			}
-			if resp.Choices[0].Delta.ReasoningContent == "" && !isAnswering {
-				fmt.Printf("\n====result content===\n")
-				isAnswering = true
-			}
-			if resp.Choices[0].Delta.ReasoningContent != "" {
-				fmt.Printf("%s", resp.Choices[0].Delta.ReasoningContent)
-			}
-			if resp.Choices[0].Delta.Content != "" {
-				fmt.Printf("%s", resp.Choices[0].Delta.Content)
-			}
+		}
+		if bVerbose && usage != nil {
+			fmt.Printf("\n\nPromptTokens: %d, CompletionTokens: %d, Total: %d\n",
+				usage.PromptTokens, usage.CompletionTokens,
+				usage.TotalTokens)
+		} else {
+			fmt.Println("")
 		}
 		stream.Close()
 		fmt.Print("> ")
